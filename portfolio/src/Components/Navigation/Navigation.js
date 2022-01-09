055b4be7d9244c4e8e080b4b3d3bfc9c9393e4a7
David An <taean2084@gmail.com>
2022-01-09 18:56:46 -0500

    footer done

--- portfolio/src/Components/Navigation/Navigation.js
+++ portfolio/src/Components/Navigation/Navigation.js
@@ -1,42 +1,7 @@
 import "./Navigation.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faReact } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-router-dom";
-import { Tabs, Tab, Box } from "@material-ui/core";
-import React, { useEffect } from "react";
 
 const Navigation = () => {
-  const { page } = params;
-  console.log(page);
-  const tabNameToIndex = {
-    0: "home",
-    1: "contact",
-    2: "portfolio",
-  };
-  const indexToTabName = {
-    home: 0,
-    contact: 1,
-    portfolio: 2,
-  };
-
-  const [selectedTab, setSelectedTab] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setSelectedTab(newValue);
-  };
-  return (
-    <div className="navbar">
-      <div className="logo">
-        <FontAwesomeIcon icon={faReact} />
-      </div>
-      <div className="nav-links">
-        <Tabs value={selectedTab} onChange={handleChange}>
-          <Tab label="Home" />
-          <Tab label="Contact" />
-          <Tab label="Portfolio" />
-        </Tabs>
-      </div>
-    </div>
-  );
+  return <div className="navbar"></div>;
 };
 
 export default Navigation;
